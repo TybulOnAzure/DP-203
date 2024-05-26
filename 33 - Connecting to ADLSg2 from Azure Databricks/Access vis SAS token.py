580f4e02ec620237505ba87651ce7ea715dc2070
Piotr Tybulewicz <piotr@tybulonazure.pl>
2024-05-26 07:57:29 +0200

    Updates

--- 33 - Connecting to ADLSg2 from Azure Databricks/Access vis SAS token.py
+++ 33 - Connecting to ADLSg2 from Azure Databricks/Access vis SAS token.py
@@ -1,7 +1,7 @@
 # Databricks notebook source
 spark.conf.set("fs.azure.account.auth.type.tybuldatalakedemo.dfs.core.windows.net", "SAS")
 spark.conf.set("fs.azure.sas.token.provider.type.tybuldatalakedemo.dfs.core.windows.net", "org.apache.hadoop.fs.azurebfs.sas.FixedSASTokenProvider")
-spark.conf.set("fs.azure.sas.fixed.token.tybuldatalakedemo.dfs.core.windows.net", "sv=2022-11-02&ss=b&srt=c&sp=rwdlacyx&se=2024-04-19T14:14:35Z&st=2024-04-11T06:14:35Z&spr=https&sig=N1wHpDi9tfMOsND0e8xL1nHK0FRCfsboXizbeX60h9I%3D")
+spark.conf.set("fs.azure.sas.fixed.token.tybuldatalakedemo.dfs.core.windows.net", "my-hardcoded-token")
 
 # COMMAND ----------
 
